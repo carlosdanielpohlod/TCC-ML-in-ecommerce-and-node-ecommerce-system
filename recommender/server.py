7999d7291a74c62136a13a4e60078741b42e070c
carlosdanielpohlod <carlospohlod@gmail.com>
2021-10-21 08:40:10 -0300

    renamed dir

--- recommender/server.py
+++ recommender/server.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_restful import Api
-
+from flask_cors import CORS
 from controllers.RecommenderController import RecommenderController
 
 app = Flask(__name__)
+CORS(app) 
 api = Api(app)
 
 
